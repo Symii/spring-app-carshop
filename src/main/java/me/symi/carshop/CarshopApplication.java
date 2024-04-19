ae624f5de466ac6794cb822928409b242ba8295f
symi <symi.contact@gmail.com>
2024-04-19 15:22:57 +0200

    Initial commit

---
+++ src/main/java/me/symi/carshop/CarshopApplication.java
@@ -0,0 +1,13 @@
+package me.symi.carshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/symi/carshop/CarshopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.symi.carshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
